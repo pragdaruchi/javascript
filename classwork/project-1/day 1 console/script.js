43876fba580aaf9e9eed6316d9813bc63695c250
ruchipragda <pragdaruchi@gmail.com>
2025-06-09 10:16:33 +0530

    Que-Ans

---
+++ classwork/project-1/day 1 console/script.js
@@ -0,0 +1,82 @@
+// console.log("Hello JS")
+// console.error("Hello JS")
+// console.warn("Hello Warn")
+
+
+// alert("This is an alert")
+// confirm("This is a conformmation box")
+// prompt("Enter Your Name :")
+
+// document.write("FDGH")
+
+
+// DAY-2
+
+// var a = 10;
+// var a = 10; //re-declare , re-initialize possible
+// var b = 20;
+
+// let c = 35;
+// // let c = 35; re-dcclare not possible
+// c= 45 //re-initialize possiblie
+
+// const d = 45;
+// d=45;//re-declaration and re-initalizzation both are not possible
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+
+// let a = 30;
+// let b = 50;
+// console.log(a+b);
+
+// let num1 = prompt("Enter num 1")
+// let num2 = prompt("Enter num 2")
+
+// alert(num1+num2)
+
+// let num3 = Number(prompt("Enter num 3"));
+// let num4 = Number(prompt("Enter num 4"));
+
+// alert(num3 + num4)
+
+
+// DAY 3//
+// Data type//
+
+// let str = "don't use this";
+// let num = 23.5;
+// let bool = true
+// let nullD = null
+// Undefind or nullD differents//
+
+// //bigint(big intezer)
+// let sym = Symbol("sym")
+// console.log(typeof(str));
+// console.log(typeof(num));
+// console.log(typeof(bool));
+// console.log(typeof(nullD));
+// console.log(typeof(sym));
+
+// let a = prompt("Enter Any Number");//string
+// let b = confirm("Confirm this or not");//boolean
+// alert(typeof(b))
+
+let a = prompt("Enter Any BirthDate");
+let b = 2025;
+alert(b-a)
+
+// Inplicit type conversion(Antometic)
+// Explicit type conversion
+
+
+// template literals
+
+// ` ->backtick
+
+let nam = "Ruchi"
+let str = `My name is ${nam}`
+console.log(str);
